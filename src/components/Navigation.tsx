459932d6230823e19f9cf0d71c0e4319beedde3b
seondal <sseondal@gmail.com>
2024-08-16 17:33:51 +0900

    ✨ feat :  페이지 구성 + 임시 네비게이션 및 검색창

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,19 @@
+// 테스트용 임시 네비게이션
+
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <div>
+      <nav>
+        <Link href={"/"}>메인</Link>
+        <Link href={"/upload"}>업로드</Link>
+        <Link href={"/mypage"}>마이페이지</Link>
+      </nav>
+      <form action="/result">
+        <input name="search" type="text" placeholder="키워드 검색" required />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
