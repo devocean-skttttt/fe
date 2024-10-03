bb6c7ea9b00c9c7f4250c8de9f58b67f03329ac7
Beongyoung Park <sergiopark10373@likelion.org>
2024-10-03 19:47:08 +0900

    :fire: Recipe Input
    레시피 등록 화면

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,36 +1,48 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation'; // Updated to use next/navigation
+import ICON from '@/constants/ICON';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 const BOTTOM_MENU = [
-  { path: '/', text: '홈' },
-  { path: '/upload', text: '등록하기' },
-  { path: '/mypage', text: '마이페이지' },
+  { path: '/', text: '홈', icon: ICON.nav.home },
+  { path: '/upload', text: '등록하기', icon: ICON.nav.upload },
+  { path: '/mypage', text: '마이페이지', icon: ICON.nav.mypage },
 ] as const;
 
 function BottomNavigation() {
-  const pathname = usePathname(); // Get the current path using usePathname
+  const pathname = usePathname();
 
-  // Check if the current path is active
   const isActive = (path: string) => pathname === path;
 
   return (
     <>
-      <div className="flex w-full items-center h-12 justify-around fixed bottom-0 bg-white max-w-mobile">
-        {BOTTOM_MENU.map((item) => (
-          <Link href={item.path} key={item.path}>
-            <div className="w-20 text-center">
-              <div
-                className={`${
-                  isActive(item.path) ? 'text-[#330218]' : 'text-[#999999]'
-                }`}
-              >
-                {item.text}
+      <div className="flex w-full items-center h-12 justify-around fixed bottom-0 bg-white max-w-mobile text-center">
+        {BOTTOM_MENU.map(({ path, text, icon }) => {
+          const isActiveItem = isActive(path);
+          const activeClass = isActiveItem
+            ? 'text-[#330218]'
+            : 'text-[#999999]';
+          const iconColor = isActiveItem ? '#330218' : '#999999';
+
+          return (
+            <Link href={path} key={path}>
+              <div className="w-20 flex flex-col items-center">
+                {icon && (
+                  <Image
+                    src={icon}
+                    alt="icon"
+                    width={24}
+                    height={24}
+                    style={{ color: iconColor }}
+                  />
+                )}
+                {text && <p className={`text-xs ${activeClass}`}>{text}</p>}
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          );
+        })}
       </div>
       <div className="h-[54px]" />
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,6 @@ function Header() {
         <div className="text-black text-[17px] font-['SF Pro'] leading-snug">
           Filter Recipe
         </div>
-        <div className="flex gap-2">
-          <div className="w-[25px] h-[13px] opacity-30 rounded border border-black" />
-          <div className="w-[21px] h-[9px] bg-black rounded-sm" />
-        </div>
       </div>
       <div className="h-[54px]" />
     </>
